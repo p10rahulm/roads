8d225f3430a3b54b659b173b8ed7e03426746d29
Rahul Madhavan <rahul@journeyoflove.in>
2023-12-20 00:01:31 +0530

    content changes

--- sync.sh
+++ sync.sh
@@ -1,16 +1,7 @@
-# Type the command "cmd < synchronizeCloudLinux.sh"
+# Type the command "cmd < sync.sh"
 # Preliminary Checks:
 # 1. Ensure "gcloud auth login" is already done
 # 2. Ensure that you are on the IISc CISCO VPN
-
-gsutil -m rsync -r assets gs://www.algorithmicfairness.site/assets
-echo "Assets Directory Synced"
-
-gsutil -m rsync -r assets gs://www.algorithmicfairness.site/content
-echo "Contents Directory Synced"
-gsutil -m rsync ./ gs://www.algorithmicfairness.site
-# gsutil cp index.html gs://www.algorithmicfairness.site/content
-echo "Base Directory Synced"
 
 
 # Now Git changes
@@ -21,7 +12,7 @@ git push
 # Now attempt to directly update website through SSH
 # Ensure local computer's ssh credentials are added to remote machine's ~/.ssh/authorized_keys
 echo "updating https://events.csa.iisc.ac.in/algorithmic-fairness-2023/"
-ssh -t events@csacloud.iisc.ac.in -p 3232 "./syncAlgFairness.sh"
-echo "Please check that the website http://www.algorithmicfairness.site/"
-echo "and https://events.csa.iisc.ac.in/algorithmic-fairness-2023/ are updated"
+ssh -t algo@csacloud.iisc.ac.in -p 3232 "./syncroads.sh"
+echo "Please check that the website http://algo.csa.iisc.ac.in/"
+echo "and https://algo.csa.iisc.ac.in/ are updated"
 # exit
