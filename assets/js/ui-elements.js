154639ae91b278e9988dc0359debf90ed9d69f75
Rahul Madhavan <rahul@journeyoflove.in>
2023-12-20 02:55:46 +0530

    css for papers

---
+++ assets/js/ui-elements.js
@@ -0,0 +1,20 @@
+function seeMoreAbstract(element) {
+    if (element.classList.contains("paper-abstract-short") && isOverflown(element)) {
+        element.classList.remove("paper-abstract-short");
+        element.classList.add("paper-abstract");
+
+
+        //create a see less token
+        const newdiv = document.createElement("div");
+        newdiv.innerHTML = "...See Less<i class=\"arrow up\"></i>";
+        newdiv.className = "paper-abstract-seeless";
+        newdiv.setAttribute("onClick", "seeLessAbstract(this)");
+        // newdiv.onclick = "seeLessAbstract(newdiv)";
+        element.parentElement.appendChild(newdiv);
+
+        seemore = element.parentElement.getElementsByClassName("paper-abstract-seemore")[0];
+        seemore.parentElement.removeChild(seemore);
+
+
+    }
+}
