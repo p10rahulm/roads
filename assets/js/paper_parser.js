d3947dc90e123047814961acfe9af75ecaa50451
Rahul Madhavan <rahul@journeyoflove.in>
2023-12-20 02:36:29 +0530

    created a paper parser

---
+++ assets/js/paper_parser.js
@@ -0,0 +1,58 @@
+function fetchPaperList() {
+    return fetch('content/papers/papers_list.txt')
+        .then(response => response.text())
+        .then(text => text.split('\n').filter(line => line.trim().length > 0));
+}
+
+function fetchPaperContent(path) {
+    return fetch(path).then(response => response.text());
+}
+
+function parsePaperContent(content) {
+    const metadata = content.split('+++')[1].trim();
+    const abstract = content.split('<b>Abstract:</b>')[1].trim().split('<div')[0].trim();
+    const title = metadata.match(/title = "(.*?)"/)[1];
+    const authors = metadata.match(/authors = "(.*?)"/)[1];
+    const publicationDetails = metadata.match(/publication_details = "(.*?)"/)[1];
+    const conferenceDate = metadata.match(/conference_date = "(.*?)"/)[1];
+    const notes = metadata.match(/notes = "(.*?)"/)[1];
+
+    return {title, authors, publicationDetails, conferenceDate, abstract, notes};
+}
+
+function createPaperHtml(paperData, fileName) {
+    return `
+        <div id="${fileName}" class="seminar seminar-fullpage">
+            <div class="paper-title">${paperData.title}</div>
+            <div class="paper-authors">${paperData.authors}</div>
+            <div class="paper-publication-details">${paperData.publicationDetails}</div>
+            <div class="paper-release-date">${paperData.conferenceDate}</div>
+
+            <div class="paper-details-holder">
+                <div class="seminar-abstract" onclick="seeMoreAbstract(this)">
+                    <b>Abstract:</b> 
+                    ${paperData.abstract}
+                </div>
+                <div class="paper-notes">${paperData.notes}</div>
+            </div>
+        </div>
+    `;
+}
+
+function updatePapersContent() {
+    fetchPaperList().then(papersList => {
+        papersList.forEach(paperPath => {
+            const fileName = paperPath.split('/').pop().replace('.md', '');
+            fetchPaperContent(paperPath)
+                .then(content => parsePaperContent(content))
+                .then(paperData => {
+                    const paperHtml = createPaperHtml(paperData, fileName);
+                    document.getElementById('papers-container').innerHTML += paperHtml;
+                });
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updatePapersContent();
+});
--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@
 
 # Now Git changes
 git add -A
-git commit -m "added research papers list"
+git commit -m "created a paper parser"
 git push
 
 # Now attempt to directly update website through SSH
