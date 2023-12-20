6f51bee2bf206b5100c9a43d08e3b72860c87573
Rahul Madhavan <rahul@journeyoflove.in>
2023-12-21 00:27:17 +0530

    split into three files.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,8 @@
-
+fetch('content/recent_news/recent_news.txt')
+    .then(response => response.text())
+    .then(text => {
+        const lines = text.split('\n');
+        const listItems = lines.map(line => line.trim() ? `<li>${line}</li>` : '').join('');
+        document.getElementById('news-content').innerHTML = `<ul>${listItems}</ul>`;
+    })
+    .catch(error => console.error('Error loading news:', error));
--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@
 
 # Now Git changes
 git add -A
-git commit -m "navbar"
+git commit -m "split into three files."
 git push
 
 # Now attempt to directly update website through SSH
