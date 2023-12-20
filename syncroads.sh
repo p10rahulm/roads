c3ce064095aeca1662c34b4d19feac76f50a1e02
Rahul Madhavan <rahul@journeyoflove.in>
2023-12-21 00:30:12 +0530

    changed syncroads.sh:

--- syncroads.sh
+++ syncroads.sh
@@ -3,6 +3,8 @@ cd roads
 git pull origin main
 echo 'fetched latest files from git'
 cp index.html /var/www/algo/index.html
+cp papers.html /var/www/algo/papers.html
+cp people.html /var/www/algo/people.html
 # cp ./* /var/www/events/algorithmic-fairness-2023/
 echo 'index file copied from base directory to hosting directory'
 cp -R ./assets /var/www/algo/
