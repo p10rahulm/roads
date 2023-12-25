5ff04ece0250314e9ef388da00f3c093135963ce
Rahul Madhavan <rahul.maddy@gmail.com>
2023-12-25 11:31:35 +0530

    Created a courses tab.

--- syncroads.sh
+++ syncroads.sh
@@ -5,6 +5,7 @@ echo 'fetched latest files from git'
 cp index.html /var/www/algo/index.html
 cp papers.html /var/www/algo/papers.html
 cp people.html /var/www/algo/people.html
+cp courses.html /var/www/algo/courses.html
 # cp ./* /var/www/events/algorithmic-fairness-2023/
 echo 'index file copied from base directory to hosting directory'
 cp -R ./assets /var/www/algo/
