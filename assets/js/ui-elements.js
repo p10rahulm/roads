3049c676376dfbb23fd144ee94910fd22ceb0778
Rahul Madhavan <rahul.maddy@gmail.com>
2024-03-25 15:15:56 +0530

    Courses Tab looking good!

--- assets/js/ui-elements.js
+++ assets/js/ui-elements.js
@@ -70,5 +70,7 @@ function handleImageNotFoundError(image) {
 }
 
 function clickLogo() {
-    window.location.href = '/'; // Assuming '/' is your home page URL
+    var currentUrl = window.location.href;
+    var baseUrl = currentUrl.substring(0, currentUrl.lastIndexOf("/")) + "/";
+    window.location.href = baseUrl;
 }
